1b3976f6bec065a7666aba3b83e0600f26302384
smarthg <118327780+smarthg@users.noreply.github.com>
2024-09-24 17:15:12 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pandas as pd
 # FastAPI Backend URL
 backend_url = "https://classify-service-xzcxv4vana-ue.a.run.app//classify"
 
-st.title("Text Classification App")
+st.title("Spiny IAB Tagger")
 
 # Load IAB Mapping CSV
 iab_mapping = pd.read_csv("iab_mapping.csv")
@@ -14,7 +14,7 @@ iab_mapping = pd.read_csv("iab_mapping.csv")
 text_input = st.text_area("Enter your text:", height=200)
 
 # Submit button
-if st.button("Classify Text"):
+if st.button("Tag Article"):
     if text_input:
         # Call the FastAPI backend to classify text
         payload = {"text": text_input}
@@ -25,7 +25,7 @@ if st.button("Classify Text"):
             
             # Display the categories
             st.subheader("Categories:")
-            for category in result[0]["categories"]:
+            for category in result[0]["categories"][:4]:
                 category_name = category['name']
                 similarity = category['similarity']
                 
@@ -38,20 +38,20 @@ if st.button("Classify Text"):
                     **Unique ID:** {iab_info['Unique ID']}  
                     **Parent:** {iab_info['Parent']}  
                     **Name:** {iab_info['Name']}  
-                    **Tier 1:** {iab_info['Tier 1']}  
+                    **Tier 1:** <span style="background-color: #ffcccb; padding: 5px; border-radius: 5px;">{iab_info['Tier 1']}</span>  
                     """
                     if pd.notna(iab_info['Tier 2']):
-                        iab_display += f"**Tier 2:** {iab_info['Tier 2']}  \n"
+                        iab_display += f"**Tier 2:** <span style='background-color: #add8e6; padding: 5px; border-radius: 5px;'>{iab_info['Tier 2']}</span>  \n"
                     if pd.notna(iab_info['Tier 3']):
-                        iab_display += f"**Tier 3:** {iab_info['Tier 3']}  \n"
+                        iab_display += f"**Tier 3:** <span style='background-color: #90ee90; padding: 5px; border-radius: 5px;'>{iab_info['Tier 3']}</span>  \n"
                     if pd.notna(iab_info['Tier 4']):
-                        iab_display += f"**Tier 4:** {iab_info['Tier 4']}  \n"
+                        iab_display += f"**Tier 4:** <span style='background-color: #ffb6c1; padding: 5px; border-radius: 5px;'>{iab_info['Tier 4']}</span>  \n"
                 else:
                     iab_display = "IAB category not found"
                 
                 # Display the category with rich UI
                 st.markdown(f"**{category_name}** (Similarity: {similarity:.2f})")
-                st.markdown(iab_display)
+                st.markdown(iab_display, unsafe_allow_html=True)
                 st.markdown("---")
         else:
             st.error("Error occurred while classifying text.")
